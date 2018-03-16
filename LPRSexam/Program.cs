0a0ababa9230a3f1a4d45fa665ee66b1be3eb007
iykim <kiydlsduq@gmail.com>
2018-03-16 14:23:08 +0900

    test
    test

--- LPRSexam/Program.cs
+++ LPRSexam/Program.cs
@@ -119,7 +119,7 @@ namespace LPRSexam
                     }
                     else
                     {
-                        
+                        //do something
                     }
 
                 }
